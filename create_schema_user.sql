be91913a85a923e39ff0797f6c0e912abb4345b4
Solutions for Oracle Cloud Infrastructure <68254337+mayur-oci@users.noreply.github.com>
2021-08-29 19:08:17 -0700

    Update create_schema_user.sql

--- create_schema_user.sql
+++ create_schema_user.sql
@@ -6,6 +6,7 @@ CREATE USER ECOMMERCE_USER IDENTIFIED BY "abcABC123!@#";
 GRANT CREATE TABLE, ALTER ANY INDEX, CREATE PROCEDURE, CREATE JOB, SELECT ANY TABLE,
       EXECUTE ANY PROCEDURE, UPDATE ANY TABLE, CREATE SESSION,UNLIMITED TABLESPACE, CONNECT, RESOURCE  
   TO ECOMMERCE_USER; 
+  
 
 -- Enable Resource Principal to Access Oracle Cloud Infrastructure Resources for db-user ECOMMERCE_USER, we just created. 
 -- For details, refer https://docs.oracle.com/en/cloud/paas/autonomous-database/adbsa/resource-principal.html
@@ -15,6 +16,7 @@ SELECT OWNER, CREDENTIAL_NAME FROM DBA_CREDENTIALS WHERE CREDENTIAL_NAME = 'OCI$
 -- To check if any other user, here ECOMMERCE_USER has access privilege, you have to check DBA_TAB_PRIVS view
 SELECT * from DBA_TAB_PRIVS WHERE DBA_TAB_PRIVS.GRANTEE='ECOMMERCE_USER';
 
+GRANT SELECT ON "SYS"."V_$PDBS" TO ECOMMERCE_USER;  
 
 
 
